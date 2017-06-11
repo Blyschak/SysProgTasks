fd4c7191775c511b4f3eff21f7fe2a05ebc772e4
Blyschak <stepanblischak@gmail.com>
2017-06-11 22:14:57 +0300

    Fix to build in output directory

--- Task3/project/output/config.h
+++ Task3/project/output/config.h
@@ -22,8 +22,5 @@
 /* Define to the version of this package. */
 #define PACKAGE_VERSION "1.0"
 
-/* Define to 1 if you have the ANSI C header files. */
-#define STDC_HEADERS 1
-
 /* Version number of package */
 #define VERSION "1.0"
